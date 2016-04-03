1102366e8d3054b93684ef506c7f013a38aaf5c4
konishi <konishi5282@gmail.com>
2016-04-03 22:09:13 +0900

    Fix Bugs about coordinates and clustering.

--- nearestNeighbor.R
+++ nearestNeighbor.R
@@ -14,19 +14,25 @@ cols <- c('red', 'blue', 'black')
 
 X <- cbind(x1, x2)
 
-xRange <- abs(X[,1]-X[,2])[1]
-yRange <- abs(X[,1]-X[,2])[2]
+xRange <- abs(X[1,1]-X[1,2])
+yRange <- abs(X[2,1]-X[2,2])
 
 if (xRange > yRange) {
   xlims <- c(min(X[1,])-3, max(X[1,])+3)
   ylims <- c(min(X[2,])-((xRange-yRange)/2+3), max(X[2,])+((xRange-yRange)/2+3))
 } else {
-  xlims <- c(min(X[2,])-((yRange-xRange)/2+3), max(X[2,])+((yRange-xRange)/2+3))
-  ylims <- c(min(X[1,])-3, max(X[1,])+3)
+  xlims <- c(min(X[1,])-((yRange-xRange)/2+3), max(X[1,])+((yRange-xRange)/2+3))
+  ylims <- c(min(X[2,])-3, max(X[2,])+3)
 }
 plot(0,0, cex=0, xlim=xlims, ylim=ylims)
-points(X[1,][1], X[2,][1], col=cols[1], pch=16, cex=2)
-points(X[1,][2], X[2,][2], col=cols[2], pch=16, cex=2)
+if ((X[2,1] > X[2,2]) || (X[2,1] == X[2,2] && X[1,1] > X[1,2])) {
+  points(X[1,1], X[2,1], col=cols[1], pch=16, cex=2)
+  points(X[1,2], X[2,2], col=cols[2], pch=16, cex=2)
+} else {
+  points(X[1,1], X[2,1], col=cols[2], pch=16, cex=2)
+  points(X[1,2], X[2,2], col=cols[1], pch=16, cex=2)
+}
+legend('topleft', legend=c('Class1','Class2','Boarder'), col=cols, pch=16)
 
 if (X[1,1]-X[1,2] == 0) {
   abline(h=(X[2,1]+X[2,2])/2)
@@ -40,8 +46,7 @@ if (X[1,1]-X[1,2] == 0) {
 
 class <- function(x, class) {
   points(x[1], x[2], pch=16, col=cols[class], cex=2)
-  cat('Class')
-  print(class)
+  cat(paste('Class',class, '\n'))
 }
 
 repeat {
@@ -50,16 +55,19 @@ repeat {
 
   # Judge which regions
   y <- (x[1]-X[1,1])^2+(x[2]-X[2,1])^2-(x[1]-X[1,2])^2-(x[2]-X[2,2])^2
+  print(y)
+  signY <- sign(-X[2,1]+X[2,2])
+  signX <- sign(-X[1,1]+X[1,2])
   if (y == 0) {
     class(x,3)
   } else if (y > 0) {
-    if (sign(-X[2,1]+X[2,2]) == 1) {
+    if ((signY == 1) || (signY == 0 && signX == 1)) {
       class(x,1)
     } else {
       class(x,2)
     }
   } else {
-    if (sign(-X[2,1]+X[2,2]) == -1) {
+    if ((signY == -1) || (signY == 0 && signX == -1)) {
       class(x,1)
     } else {
       class(x,2)
